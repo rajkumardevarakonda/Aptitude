640ea98abb861418a2906b230ab445c864a25cf8
D.RAJKUMAR <rajkumar23dev@gmail.com>
2023-05-04 17:17:13 +0530

    commit

--- StockShares.py
+++ StockShares.py
@@ -1,3 +1,4 @@
+from fractions import Fraction
 def find_numberofshares():
     ti = float(input("Enter the TotalIncome : "))
     inc = float(input("Enter the Income on EachShare : "))
@@ -5,8 +6,36 @@ def find_numberofshares():
     nsv = float(ti / (inc + brok))
     print("Number of ShareValues : " + str(nsv))
 
-given_data = input("No of ShareValues enter 1")
-if given_data == '1':
+def find_TotalDebtre():
+    tin = float(input("Total Income : "))
+    ei = float(input("each income on face value : "))
+    x = float(input("cost of each debenture : "))
+    y = float(input("Brokerage : "))
+    nd = float(tin / ei)
+    ced = float(x + ((y * x)/100))
+    tid =(ced * nd)
+    print("T.Investment on debentures : " + str(tid))
+
+def find_interest_obtained_on_FaceValue():
+    ns = float(input("No of Shares : "))
+    esv = float(input("each share value : "))
+    d = float(input("Discount : "))
+    invt = float(ns*(esv-d))
+    print("investment :" + str(invt))
+    fv = float(ns*esv)
+    print("Face Value :" + str(fv))
+    dvdp = Fraction(input("Dividend % : "))
+    dvd = Fraction((dvdp / 100)*fv)
+    interestObtained = float(dvd*100)/invt
+    print("Interest Obtained :" + str(interestObtained) + "%")
+
+
+given_data = input("No of SV enter 1 or Total_Debenture enter 2 or Interest_on_faceValue enter 3 : ")
+if  given_data == "1":
    find_numberofshares()
+elif given_data == "2":
+   find_TotalDebtre()
+elif given_data == "3":
+   find_interest_obtained_on_FaceValue()
 else:
- print('enter a valid input')
+   print('enter a valid input')
--- Stocks&Shares.py
+++ Stocks&Shares.py
@@ -1,15 +1,16 @@
-def find_costprice():
+def find_cost_price():
     fv = float(input("Enter the FaceValue : "))
     dis = float(input("Enter the Discount : "))
     brok= float(input("Enter the Brokerage : "))
     cp = float(fv - dis + brok)
-    print("CostPrice : " + str(cp))
+    print("costPrice : " + str(cp))
 
-def find_stockvalue():
+
+def find_stock_value():
     sp = float(input("Enter the SellingPrice : "))
     brok = float(input("Enter the Brokerage : "))
     sv = float(sp - brok)
-    print("StockValue : " + str(sv))
+    print("stockValue : " + str(sv))
 
 
 def find_investment():
@@ -18,7 +19,6 @@ def find_investment():
     ic2 = float(input("Income2 or Earn2 or obtain2 : "))
     iv2 = float((iv1 / ic1) * ic2)
     print("Investment2 or Stock2 :" + str(iv2))
-     # find_stockvalue()
 
 
 def find_dividend():
@@ -28,34 +28,35 @@ def find_dividend():
     inc2 = float((inc1 / i1) * i2)
     print("Income2 : " + str(inc2))
     print("Dividend :" + str(inc2) + "%")
-    # find_investment()
 
-def find_annualincome():
+
+def find_annual_income():
     ti = float(input("Total Investment : "))
     inv1 = float(input("Investment in 1'share : "))
     fc1 = float(input("FaceValue of 1'Share : "))
     dvd = float(input("Dividend : "))
-    ai = float(dvd * ((ti / inv1) * fc1))
+    ai = float((dvd /100 ) * ((ti / inv1) * fc1))
     print("Annual_Income : " + str(ai))
 
- def find_income():
-     ivt1 = float(input("Investment1 :"))
-     incm1 = float(input("Income1: "))
-     ivt2 = float(input("Investment2 : "))
-     incm2 = float((incm1 / ivt1) * ivt2)
-     print("Income2 :" + str(incm2))
+
+def find_income():
+    ivt1 = float(input("Investment1 :"))
+    income1 = float(input("Income1: "))
+    ivt2 = float(input("Investment2 : "))
+    income2 = float((income1 / ivt1) * ivt2)
+    print("Income2 :" + str(income2))
 
 given_data = input("CP enter 1. or Sold_SV enter 2. or INV enter 3. or DVD enter 4. or A.I enter 5. or INC enter 6.")
 if given_data == '1':
- find_costprice()
+  find_cost_price()
 elif given_data == '2':
- find_stockvalue()
+  find_stock_value()
 elif given_data == '3':
   find_investment()
 elif given_data == '4':
  find_dividend()
 elif given_data == '5':
- find_annualincome()
+ find_annual_income()
 elif given_data == '6':
  find_income()
 else:
